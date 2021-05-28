4a0bf795acc35ce3fe9a8fb9bd951f823e4e588e
javalikescript <javalikescript@free.fr>
2021-05-28 22:03:23 +0200

    Add example with callback

---
+++ webview-example-2.c
@@ -0,0 +1,26 @@
+#define WEBVIEW_IMPLEMENTATION
+/* don't forget to define WEBVIEW_WINAPI,WEBVIEW_GTK or WEBVIEW_COCAO */
+#include "webview.h"
+
+static void invoke_cb(struct webview *w, const char *arg) {
+  printf("Callback called with '%s'\n", arg);
+}
+
+int main(int argc, char *argv[]) {
+  struct webview webview;
+  int r;
+	webview.url = "data:text/html,<!DOCTYPE html><html><body>"
+    "<p id=\"sentence\">It works !</p>"
+    "<button onclick=\"window.external.invoke('Hi')\">Callback</button>"
+    "</body></html>";
+	webview.title = "Simple webview example";
+	webview.width = 600;
+	webview.height = 400;
+	webview.resizable = 1;
+  webview.external_invoke_cb = &invoke_cb;
+  r = webview_init(&webview);
+	do {
+		r = webview_loop(&webview, 1);
+	} while (r == 0);
+  return 0;
+}
